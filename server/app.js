18124a4a2f4c079ecea09ea1588227f893414b90
OyewoleOyedeji <oyedejioyewolemaxwell@gmail.com>
2022-05-06 20:01:31 +0100

    Performed security enhancements
    Based on CodeQL scan.
    1. npm: Added express-rate-limit
    2. Made changes to ./README.md
    3. Edited server/app.js - enabling rate limiting which protects against DDOS attacks

--- server/app.js
+++ server/app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const { default: rateLimit } = require('express-rate-limit')
 
 const indexRouter = require('./routes/index')
 const apiRouter = require('./routes/api')
 
 const app = express()
+
+/**
+ * Limit concurrent request to all server routes
+ * to 10 per minute
+ */
+let limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 10,
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false,
+})
+
+app.use(limiter)
 
 app.use(logger('dev'))
 app.use(express.json())
